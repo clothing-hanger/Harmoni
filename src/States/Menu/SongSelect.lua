43dd9bf96ef300576fa8bcdc0565272c501cf68a
clothing-hanger <kelbymixon1@gmail.com>
2024-08-07 18:15:22 -0500

    :(

--- src/States/Menu/SongSelect.lua
+++ src/States/Menu/SongSelect.lua
@@ -4,27 +4,23 @@ local MenuState
 
 function SongSelect:enter()
     MenuState = "Song"
-
     SelectedSong = 1
     SelectedDifficulty = 1
-
     DifficultyList = {}
-    SelectedDifficulty = 1
-
-
 end
 
 function SongSelect:update(dt)
     if Input:pressed("menuDown") then
-        SelectedSong = plusEq(SelectedSong)
-        SelectedDifficulty = plusEq(SelectedDifficulty)
+        SelectedSong = (SelectedSong % #SongList) + 1
+        SelectedDifficulty = (SelectedDifficulty % #DifficultyList) + 1
     elseif Input:pressed("menuUp") then
-        SelectedSong = minusEq(SelectedSong)
-        SelectedDifficulty = minusEq(SelectedDifficulty)
+        SelectedSong = (SelectedSong - 2) % #SongList + 1
+        SelectedDifficulty = (SelectedDifficulty - 2) % #DifficultyList + 1
     elseif Input:pressed("menuConfirm") then
         if MenuState == "Song" then
             MenuState = "Difficulty"
             SongSelect:setupDifficultyList()
+            SelectedDifficulty = 1
         elseif MenuState == "Difficulty" then
             SongString = "Music/" .. SongList[SelectedSong] .. "/" .. DifficultyList[SelectedDifficulty]
             print(SongString)
@@ -32,18 +28,12 @@ function SongSelect:update(dt)
         end
     end
 
-    if SelectedSong > #SongList then SelectedSong = 1 end
-    if SelectedSong < 1 then SelectedSong = #SongList end
-    
-    if SelectedDifficulty > #DifficultyList then SelectedDifficulty = 1 end
-    if SelectedDifficulty < 1 then SelectedDifficulty = #DifficultyList end
 end
 
 function SongSelect:setupDifficultyList()
     DifficultyList = {}
     local SongContents = love.filesystem.getDirectoryItems("Music/" .. SongList[SelectedSong])
-    for i = 1,#SongContents do
-        print(getFileExtension(tostring(SongContents[i])))
+    for i = 1, #SongContents do
         if getFileExtension(tostring(SongContents[i])) == ".qua" then
             table.insert(DifficultyList, SongContents[i])
         end
@@ -52,17 +42,16 @@ end
 
 function SongSelect:draw()
     if MenuState == "Song" then
-        for i = 1,#SongList do
-            if i == SelectedSong then love.graphics.setColor(0,1,1) else love.graphics.setColor(1,1,1) end
-            love.graphics.print(SongList[i], 300, (100)+(15*i))
+        for i = 1, #SongList do
+            if i == SelectedSong then love.graphics.setColor(0, 1, 1) else love.graphics.setColor(1, 1, 1) end
+            love.graphics.print(SongList[i], 300, 100 + (15 * i))
         end
     elseif MenuState == "Difficulty" then
-        for i = 1,#DifficultyList do
-            if i == SelectedDifficulty then love.graphics.setColor(0,1,1) else love.graphics.setColor(1,1,1) end
-            love.graphics.print(DifficultyList[i], 300, (100)+(15*i))
+        for i = 1, #DifficultyList do
+            if i == SelectedDifficulty then love.graphics.setColor(0, 1, 1) else love.graphics.setColor(1, 1, 1) end
+            love.graphics.print(DifficultyList[i], 300, 100 + (15 * i))
         end
     end
-
 end
 
 return SongSelect
