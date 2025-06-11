6cb26a163e5f78733ab85d189458587414a21a67
clothing-hanger <kelbymixon1@gmail.com>
2025-06-10 22:13:37 -0500

    response.

--- src/states/game/gameModeManager.lua
+++ src/states/game/gameModeManager.lua
@@ -3,6 +3,7 @@ local gameModeManager = State()
 function gameModeManager:enter(s,mode,chart)
     self.gameMode = {} -- i hate that this has to be a table 😭😭
                        -- Literally why does it have to be a table?
+                       -- because its FUNNY guglio,,,,, but you would never understand
     if mode == "mania" then
         self.gameMode = {mania(chart)}
     end
