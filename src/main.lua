6128a5d529f7c9c18487b47ad247b28a5de86a5c
clothing-hanger <harmonigame@gmail.com>
2025-07-23 12:13:38 -0500

    i think nothing changed? idfk

--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,7 @@ require("love.error")
 require("love.run")
 
 
-dontShowBG = false  -- i can NOT work on this game anywhere people can see my laptop cuz these fucking weird ass backgrounds FUCK YOU QUAVER AND OSU MAPPERS WHAT IS WRONG WITH YOU FUCKING PEOPLE
+dontShowBG = true  -- i can NOT work on this game anywhere people can see my laptop cuz these fucking weird ass backgrounds FUCK YOU QUAVER AND OSU MAPPERS WHAT IS WRONG WITH YOU FUCKING PEOPLE
 
 local function table_find(t, value)
     for i, v in ipairs(t) do
