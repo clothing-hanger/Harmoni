43d16e0cd3aaa7c9a594d3a8746258cc624378ce
clothing-hanger <kelbymixon1@gmail.com>
2025-12-04 23:20:35 -0600

    i forgot to fucking turn off america mode 💔

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,7 @@ require("love.init")
 -- ass backgrounds FUCK YOU QUAVER AND OSU MAPPERS WHAT IS WRONG WITH YOU FUCKING PEOPLE
 dontShowBG = false
 
-AMERICA =  true
+AMERICA =  false
 
 spongebirth = love.graphics.newImage("images/spongebirth.png")
 
