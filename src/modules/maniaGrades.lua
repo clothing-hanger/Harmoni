66bd27ea0aa674112ef9e1a02d3afc1974988481
clothing-hanger <kelbymixon1@gmail.com>
2026-01-09 10:27:30 -0600

    finally replaced Slapstick's lyrics file

--- src/modules/maniaGrades.lua
+++ src/modules/maniaGrades.lua
@@ -12,7 +12,7 @@ end
 
 maniaGrades.getGrades_PLURAL_that_is_grades_WITH_AN_S__this_function_gives_you_the_TABLE_of_grades___this_is_NOT_the_one_that_gives_the_current_grade_when_you_pass_in_an_accuracy___that_is_the_OTHER_similarly_named_function_called_getGrade__that_is_not_THIS_one = function()
     return {
-        {grade = "S+", accuracy = 100},
+        {grade = "H", accuracy = 100},
         {grade = "S", accuracy = 95},
         {grade = "A", accuracy = 90},
         {grade = "B", accuracy = 80},
