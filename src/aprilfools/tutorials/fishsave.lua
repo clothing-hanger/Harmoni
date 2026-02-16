7bf980588f7203344448887dd6bfa62e126a81f1
clothing-hanger <kelbymixon1@gmail.com>
2026-02-16 16:26:59 -0600

    some bullshit

---
+++ src/aprilfools/tutorials/fishsave.lua
@@ -0,0 +1,37 @@
+return {
+    meta = {
+        title = "The Fish Save",
+    },
+    pages = {
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/when no fish save.png"),
+            title = "You won't get far without knowing how to do the fish save.",
+            text = "The fish save is the action of using a fish to save yourself."
+        },
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/how do fish save.png"),
+            title = "How does one perform the fish save?",
+            text = "To perform the fish save, you will need to use a fish to save yourself. You will need at least 1 fish."
+        },
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/what is fish.png"),
+            title = "What is a fish?",
+            text = "Fish [fiSH]\nA limbless cold-blooded vertebrate animal with gills and fins and living wholly in water:\n   \"the flesh of fish as food\"\n   \"hot crab appetizers stuffed with fish\""
+        },
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/what is save.png"),
+            title = "What is a save?",
+            text = "Save [sāv]\nKeep safe or rescue (someone or something) from harm or danger:\n   \"they used a fish to save themselves\""
+        },
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/why learn.png"),
+            title = "Why should you learn the fish save?",
+            text = "the fish save will save you. saving you means you will be saved. saved by the fish. saved by the fish by performing the fish save. you need to learn the fish save"
+        },        
+        {
+            image = love.graphics.newImage("aprilfools/images/fishsave/what is fish.png"),
+            title = "What is a fish?",
+            text = "Fish [fiSH]\nA limbless cold-blooded vertebrate animal with gills and fins and living wholly in water:\n   \"the flesh of fish as food\"\n   \"hot crab appetizers stuffed with fish\""
+        },
+    }
+}
